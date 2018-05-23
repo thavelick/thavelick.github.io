4cd889a5d013b77dfeb7e57a3af7070256970f0d
Tristan Havelick <tristan@havelick.com>
2018-05-22 21:32:02 -0600

    Remove extraneous junk form template site
    Also fix minor syntax issues

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/header'
-import './index.css'
+import Header from '../components/header';
+import './index.css';
 
 const Layout = ({ children, data }) => (
   <div>
@@ -20,19 +20,19 @@ const Layout = ({ children, data }) => (
         margin: '0 auto',
         maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        paddingTop: 0
       }}
     >
       {children()}
     </div>
   </div>
-)
+);
 
 Layout.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default Layout
+export default Layout;
 
 export const query = graphql`
   query SiteTitleQuery {
@@ -42,4 +42,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
