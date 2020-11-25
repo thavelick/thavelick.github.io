925c8672c43f47b4ab5d6c0280f69c316e18cff5
Tristan Havelick <tristan@havelick.com>
2020-11-24 21:45:00 -0700

    #16 add breadcrumbs
    Also fix article tag to surround content

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
+import Link from "gatsby-link";
+
 export default function Template({ data }) {
   const { markdownRemark: post } = data
   const title = post.frontmatter.title;
   return (
     <Layout title={title}>
-        <article></article>
-        <h1>{title}</h1>
-        <small>Posted {post.frontmatter.date}</small>
-        <div
-          className="article-content"
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        />
+        <Link to="/blog">Articles</Link>
+        <article>
+          <h1>{title}</h1>
+          <small>Posted {post.frontmatter.date}</small>
+          <div
+            className="article-content"
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          />
+        </article>
     </Layout>
   );
 };
