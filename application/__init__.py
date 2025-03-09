fb85529143c543d84b179bd628cad44e5e1aebc2
Tristan Havelick <tristan@havelick.com>
2025-03-08 22:44:56 -0700

    run black, move import to top

--- application/__init__.py
+++ application/__init__.py
@@ -1,18 +1,22 @@
-"""Application package."""
+"""Flask website for TristanHavelick.com."""
+
 import os
 from flask import Flask
 from werkzeug.exceptions import NotFound
+
+from . import db
+
 
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'blog.db'),
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "blog.db"),
     )
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
 
@@ -21,24 +25,22 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/hello')
+    @app.route("/hello")
     def hello():
-        return 'Hello, World!'
+        return "Hello, World!"
 
-    @app.route('/', defaults={'path': 'index.html'})
-    @app.route('/<path:path>')
+    @app.route("/", defaults={"path": "index.html"})
+    @app.route("/<path:path>")
     def static_proxy(path):
         try:
             return app.send_static_file(path)
         except NotFound as e:
-            new_path = os.path.join(path, 'index.html')
+            new_path = os.path.join(path, "index.html")
             full_path = os.path.join(app.static_folder, new_path)
             if os.path.exists(full_path):
                 return app.send_static_file(new_path)
             raise NotFound() from e
 
-    # pylint: disable=import-outside-toplevel
-    from . import db
     db.init_app(app)
 
     return app
