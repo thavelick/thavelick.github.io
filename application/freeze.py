190bf0ac1a7fa752db92c5987209cd2e411bcea8
Tristan Havelick <tristan@havelick.com>
2025-03-10 19:37:09 -0600

    crawl posts with trailing slash to make flash freeze correctly make index.html files

--- application/freeze.py
+++ application/freeze.py
@@ -1,3 +1,4 @@
+import os
 from flask_frozen import Freezer
 from application import create_app
 from application.models import Post
@@ -9,14 +10,16 @@ freezer = Freezer(app)
 @freezer.register_generator
 def catchall():
     for post in Post.fetch_all():
-        yield {"path": post["slug"]}
-    
-    import os
+        yield {"path": f"{post["slug"]}/"}
+
     archive_dir = os.path.join("application", "static", "archive")
-    for root, dirs, files in os.walk(archive_dir):
+    for root, _, files in os.walk(archive_dir):
         for file in files:
-            relative_path = os.path.relpath(os.path.join(root, file), "application/static")
+            relative_path = os.path.relpath(
+                os.path.join(root, file), "application/static"
+            )
             yield {"path": relative_path}
+
 
 if __name__ == "__main__":
     freezer.freeze()
